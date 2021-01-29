3e18a6a0a7c960e583b278778a723749e4e00d1e
giwa <ugw.gi.world@gmail.com>
2021-01-29 15:31:44 +0900

    remove unused code

--- src/main/java/org/embulk/input/postgresql_wal/PostgresqlWalClient.java
+++ src/main/java/org/embulk/input/postgresql_wal/PostgresqlWalClient.java
@@ -21,34 +21,6 @@ public class PostgresqlWalClient {
         return dbmd.getDatabaseMajorVersion();
     }
 
-    public String getCurrentWalLSN() throws SQLException {
-        String sql = "SELECT pg_current_wal_lsn()"; // SQL for PG 10+
-        if (getMajorVersion() <= 9) {
-            sql = "SELECT pg_current_xlog_location()";
-        }
-        Statement stmt = con.createStatement();
-        ResultSet rs = stmt.executeQuery(sql);
-        while (rs.next()) {
-            return rs.getString(1);
-        }
-        // never reach here
-        throw new RuntimeException("Could not fetch LSN");
-    }
-
-    public Map<String, String> getColumns(String schemaName, String tableName) throws SQLException {
-        String sql = "SELECT column_name, data_type  FROM information_schema.columns WHERE table_schema = ? AND table_name = ?";
-        PreparedStatement pstmt = con.prepareStatement(sql);
-        pstmt.setString(1, schemaName);
-        pstmt.setString(2, tableName);
-        ResultSet rs = pstmt.executeQuery();
-        Map<String, String> schema = new HashMap<>();
-
-        while (rs.next()) {
-            schema.put(rs.getString("column_name"), rs.getString("data_type"));
-        }
-        return schema;
-    }
-
     public PGReplicationStream getReplicationStream(String slotName, String fromLsn) throws SQLException {
         PGConnection pgConnection = con.unwrap(PGConnection.class);
         ChainedLogicalStreamBuilder builder = pgConnection.getReplicationAPI().replicationStream().logical().withSlotName(slotName)
